988e1504fcf5d0a34810db1b2c6769cf6dbc13ea
Radcabna <radcabna@gmail.com>
2026-01-20 20:14:27 +0300

    first commit

---
+++ Cosplay Mania Planner/Views/Loading.swift
@@ -0,0 +1,18 @@
+//
+//  Loading.swift
+//  Cosplay Mania Planner
+//
+//  Created by Алкександр Степанов on 20.01.2026.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loading()
+}
---
+++ Cosplay Mania Planner/Views/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  Cosplay Mania Planner
+//
+//  Created by Алкександр Степанов on 20.01.2026.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}

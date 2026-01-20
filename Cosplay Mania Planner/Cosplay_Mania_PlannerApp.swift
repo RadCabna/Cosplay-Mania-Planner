33946493c3b0a049993d4bc3e31292050b6f3d7a
Radcabna <radcabna@gmail.com>
2026-01-20 18:02:10 +0300

    Initial Commit

---
+++ Cosplay Mania Planner/Cosplay_Mania_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosplay_Mania_PlannerApp.swift
+//  Cosplay Mania Planner
+//
+//  Created by Алкександр Степанов on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Cosplay_Mania_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
